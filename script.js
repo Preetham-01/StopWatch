2f5ff112029922028c3a8846f6cdfa29e3a7b0e6
Preetham Seelam <113579073+Preetham-01@users.noreply.github.com>
2024-07-14 23:39:46 +0530

    script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let stopBtn = document.getElementById('stopBtn');
 let startBtn = document.getElementById('startBtn');
 let resetBtn = document.getElementById('resetBtn');
 
+
 let msec = 0o0;
 let secs = 0o0;
 let mins = 0o0;
